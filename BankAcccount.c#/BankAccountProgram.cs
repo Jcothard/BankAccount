1f5ac13c79c8769c00515997b5a8ead8cede8e49
Josh Cothard <jcothard@yahoo.com>
2020-03-03 06:32:47 -0500

    update

---
+++ BankAcccount.c#/BankAccountProgram.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("JJ", 1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+        }
+    }
+}
